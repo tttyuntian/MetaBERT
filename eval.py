27dcc7e3b0dd15cc5981788864ad547681794bfa
sinanspd <sinanspd@gmail.com>
2020-12-02 00:18:48 -0500

    fine tune

--- eval.py
+++ eval.py
@@ -5,7 +5,11 @@ from main import get_logger, get_classifiers, get_train_steps, support_query_spl
 from datasets import load_dataset, load_metric
 from modules.preprocess import get_label_lists, preprocess, get_num_labels, get_dataloaders
 from transformers import AdamW
+from modules.meta_learning import sample_task
+from copy import deepcopy
+import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def parseArguments(): 
     parser = argparse.ArgumentParser()
@@ -26,6 +30,7 @@ def parseArguments():
     parser.add_argument("--num_update_steps", type=int, default=20)
     parser.add_argument("--num_sample_tasks", type=int, default=8)
     parser.add_argument("--inner_learning_rate", type=float, default=1e-3)
+    parser.add_argument("--config-path", type=str, default="./metebert-small-tuned/")
 
     return parser.parse_args()
 
@@ -51,7 +56,6 @@ def main(args):
     num_labels     = get_num_labels(label_lists)
     train_datasets  = preprocess(train_datasets, tokenizer, args)
 
-
     print("Retrieving support and query sets.")
     support_datasets, query_datasets = support_query_split(train_datasets, 0.2)
     support_dataloaders = get_dataloaders(support_datasets, "support", args)
@@ -60,25 +64,39 @@ def main(args):
     print("In Train Mode")
 
     model.train()
-    optimizer = AdamW(model.parameters(), lr=1e-5)
     classifiers = get_classifiers(num_labels, args)
 
     train_steps_per_task = get_train_steps(support_dataloaders, args)
 
-    #for epoch in range(args.epochs):
+    for epoch_id in range(args.num_train_epochs):
+        print(epoch_id)
+        sample_task_ids = sample_task(train_steps_per_task, args)
+        #print(sample_task_ids)
+        #print(classifiers)
+        classifier = classifiers[0]
+        classifier.embedder = deepcopy(model)
+        classifier.to(device)
+        optimizer = AdamW(classifier.parameters(), lr=1e-5)
+        classifier.train()
 
+        #print(support_dataloaders)
+        support_dataloader = support_dataloaders[0]
 
-    #fine tuning
+        all_loss = []
+        print("starting to step")
+        for step_id in range(args.num_update_steps):
+            batch = next(iter(support_dataloader))
+            input_ids, attention_mask, token_type_ids, labels = tuple(t.to(device) for t in batch)
+            print(labels)
+            outputs = classifier(input_ids, attention_mask, token_type_ids, labels = labels)
+            loss = outputs[1]
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            all_loss.append(loss.item())
 
+        torch.save(classifier, args.config-path)
 
-    #tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True)
-    #test_datasets  = preprocess({args.task: test}, tokenizer, args)
-
-
-    # fine tune the model 
-    # train for preset epoch 
-
-    
     print("hello")
 
 
